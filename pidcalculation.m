a2fdaece306663c8541d8ff515d25095747e3c4d
WELCOME <WELCOME@MSI>
2020-12-12 21:44:00 +0530

    by changing commands in script , step disturbance is plotted

--- pidcalculation.m
+++ pidcalculation.m
@@ -13,10 +13,16 @@ for i = 1:3
     C(:,:,i) = pid(Kp,Ki,Kd);
     Kd = Kd + 0.1;
 end
+% dist_cl = feedback(P_motor,C);
+% t = 0:0.001:0.2;
+% step(dist_cl(:,:,1), dist_cl(:,:,2), dist_cl(:,:,3), t)
+% ylabel('Position, \theta (radians)')
+% title('Step Response with K_p = 21, K_i = 500 and Different values of K_d')
+% legend('Kd = 0.05', 'Kd = 0.15', 'Kd = 0.25')
 
-sys_cl = feedback(C*P_motor,1);
-t = 0:0.001:0.1;
-step(sys_cl(:,:,1), sys_cl(:,:,2), sys_cl(:,:,3), t)
+dist_cl = feedback(P_motor,C);
+t = 0:0.001:0.2;
+step(dist_cl(:,:,1), dist_cl(:,:,2), dist_cl(:,:,3), t)
 ylabel('Position, \theta (radians)')
-title('Step Response with K_p = 21, K_i = 500 and Different Values of K_d')
+title('Step Disturbance Response with K_p = 21, K_i = 500 and Different values of K_d')
 legend('Kd = 0.05', 'Kd = 0.15', 'Kd = 0.25')
